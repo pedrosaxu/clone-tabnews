3b3e3c93f31165e9459aa48e73a2c5530e8925e0
Xu <101535045+pedrosaxu@users.noreply.github.com>
2023-11-10 00:49:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # clone-tabnews
+
+Implementação do https://tabnews.com.br como parte do curso https://curso.dev 🚀
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Mozinho, I'm lucky to have you. 💖</h1>
+  return <h1>Mozinho, I'm lucky to have you. 💖</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
+
+function test() {
+  console.log("oi");
+}
